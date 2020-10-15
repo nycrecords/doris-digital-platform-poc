331fb36743f29bb0cd8129614bb76ec37453ea01
Joel Castillo <jocastillo@records.nyc.gov>
2020-10-15 14:30:25 -0400

    Add rule to NSG

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -60,6 +60,21 @@ data "azurerm_network_security_group" "nsg" {
   resource_group_name = data.azurerm_resource_group.rg-network.name
 }
 
+resource "azurerm_network_security_rule" "example" {
+  name                                       = "test123"
+  priority                                   = 100
+  direction                                  = "Outbound"
+  access                                     = "Allow"
+  protocol                                   = "Tcp"
+  source_port_range                          = "*"
+  destination_port_range                     = "*"
+  source_address_prefix                      = "*"
+  destination_address_prefix                 = "*"
+  destination_application_security_group_ids = [azurerm_application_security_group.example.id]
+  resource_group_name                        = data.azurerm_resource_group.rg-network.name
+  network_security_group_name                = data.azurerm_network_security_group.nsg.name
+}
+
 ### ASG
 resource "azurerm_application_security_group" "example" {
   name                = "tf-appsecuritygroup"
